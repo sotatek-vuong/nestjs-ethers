7591b8e0ac0fa4d64afdf0b10629be3471146819
sotatek-vuong <hoang.vuong@sotatek.com>
2022-11-15 15:07:53 +0700

    migration fetch func

--- src/kcc/kccscan-provider.ts
+++ src/kcc/kccscan-provider.ts
@@ -1,4 +1,6 @@
+import { showThrottleMessage } from '@ethersproject/providers'
 import { ethers } from 'ethers'
+import { ConnectionInfo, deepCopy, fetchJson } from 'ethers/lib/utils'
 
 import { getNetwork } from './networks'
 
@@ -6,6 +8,70 @@ import { version } from './_version'
 const logger = new ethers.utils.Logger(version)
 
 const defaultApiKey = 'EVTS3CU31AATZV72YQ55TPGXGMVIFUQ9M9'
+
+function getJsonResult(result: {
+  jsonrpc: string
+  result?: any
+  error?: { code?: number; data?: any; message?: string }
+}): any {
+  // This response indicates we are being throttled
+  if (
+    result &&
+    (<any>result).status == 0 &&
+    (<any>result).message == 'NOTOK' &&
+    (result.result || '').toLowerCase().indexOf('rate limit') >= 0
+  ) {
+    const error: any = new Error('throttled response')
+    error.result = JSON.stringify(result)
+    error.throttleRetry = true
+    throw error
+  }
+
+  if (result.jsonrpc != '2.0') {
+    // @TODO: not any
+    const error: any = new Error('invalid response')
+    error.result = JSON.stringify(result)
+    throw error
+  }
+
+  if (result.error) {
+    // @TODO: not any
+    const error: any = new Error(result.error.message || 'unknown error')
+    if (result.error.code) {
+      error.code = result.error.code
+    }
+    if (result.error.data) {
+      error.data = result.error.data
+    }
+    throw error
+  }
+
+  return result.result
+}
+
+function getResult(result: { status?: number; message?: string; result?: any }): any {
+  // getLogs, getHistory have weird success responses
+  if (
+    result.status == 0 &&
+    (result.message === 'No records found' ||
+      result.message === 'No transactions found' ||
+      result.message === 'No internal transactions found' ||
+      (result.message?.includes('No') && result.message.includes('found')))
+  ) {
+    return result.result
+  }
+
+  if (result.status != 1 || result.message != 'OK') {
+    const error: any = new Error('invalid response')
+    error.result = JSON.stringify(result)
+    if ((result.result || '').toLowerCase().indexOf('rate limit') >= 0) {
+      error.throttleRetry = true
+    }
+    throw error
+  }
+
+  return result.result
+}
 
 export class KccscanProvider extends ethers.providers.EtherscanProvider {
   constructor(network?: ethers.providers.Networkish, apiKey?: string) {
@@ -22,6 +88,50 @@ export class KccscanProvider extends ethers.providers.EtherscanProvider {
     }
 
     super(<ethers.providers.Network>standardNetwork, apiKey || defaultApiKey)
+  }
+  async fetch(module: string, params: Record<string, any>, post?: boolean | undefined): Promise<any> {
+    const url = post ? this.getPostUrl() : this.getUrl(module, params)
+    const payload = post ? this.getPostData(module, params) : null
+    const procFunc = module === 'proxy' ? getJsonResult : getResult
+
+    this.emit('debug', {
+      action: 'request',
+      request: url,
+      provider: this,
+    })
+
+    const connection: ConnectionInfo = {
+      url: url,
+      throttleSlotInterval: 1000,
+      throttleCallback: (attempt: number, url: string) => {
+        if (this.isCommunityResource()) {
+          showThrottleMessage()
+        }
+        return Promise.resolve(true)
+      },
+    }
+
+    // @ts-ignore
+    let payloadStr: string = null
+    if (payload) {
+      connection.headers = { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+      payloadStr = Object.keys(payload)
+        .map((key) => {
+          return `${key}=${payload[key]}`
+        })
+        .join('&')
+    }
+
+    const result = await fetchJson(connection, payloadStr, procFunc || getJsonResult)
+
+    this.emit('debug', {
+      action: 'response',
+      request: url,
+      response: deepCopy(result),
+      provider: this,
+    })
+
+    return result
   }
 
   getBaseUrl(): string {
